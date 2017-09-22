6759f50fdbfc0880feaa58ff5da0e586e5438c5c
subrata9237 <bhuyan.subrata.bhuyan@gmail.com>
2017-09-22 10:46:14 -0700

    ggg

---
+++ Learning/Learning/assets/Scripts/Player.js
@@ -0,0 +1,24 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // main character's jump height
+        jumpHeight: 0,
+        // main character's jump duration
+        jumpDuration: 0,
+        // maximal movement speed
+        maxMoveSpeed: 0,
+        // acceleration
+        accel: 0,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+    
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
